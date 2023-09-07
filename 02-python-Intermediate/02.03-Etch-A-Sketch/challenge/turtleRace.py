515c56f25d2a57df0d9277f65845d456b55c2cc6
Hamdrew <141601957+hamdrew-jl@users.noreply.github.com>
2023-09-07 18:20:04 +0800

    Update turtleRace.py

--- 02-python-Intermediate/02.03-Etch-A-Sketch/challenge/turtleRace.py
+++ 02-python-Intermediate/02.03-Etch-A-Sketch/challenge/turtleRace.py
@@ -9,10 +9,18 @@ y_position = [-150, -90, -30, 30, 90, 150]
 screen.bgcolor("dark sea green")
 
 
+def title():
+    """Print Game title"""
+    t.penup()
+    t.hideturtle()
+    t.goto(-80, 175)
+    t.write("Welcome to The Race!", font=("Arialblack", 10))
+
+
 def athletic_track():
     """create athletic track"""
     track = Turtle()
-    track.speed(0)
+    track.speed("fastest")
     for y in range(-180, 210, 60):
         for x in range(-280, 310, 60):
             track.color("white smoke")
@@ -23,18 +31,36 @@ def athletic_track():
             track.forward(100)
 
 
-# bring up popup for user bet
+def finish_flag():
+    """create finish flag"""
+    flag = Turtle("square")
+    flag.penup()
+    flag.speed("fastest")
+    x = 200
+    y = 180
+
+    for i in range(9):
+        flag.goto(x, y)
+        flag.stamp()
+        x += 20
+        y -= 20
+        flag.goto(x, y)
+        flag.stamp()
+        x -= 20
+        y -= 20
+        flag.goto(x, y)
+        flag.stamp()
+
+
+# initial preparation
+athletic_track()
+finish_flag()
+title()
+
+# Popup for user bet
 user_bet = screen.textinput(title="Make your bet",prompt="Which turtle will win the race? Enter a color: ").lower()
 
-athletic_track()
-
-# title
-t.penup()
-t.hideturtle()
-t.goto(-80, 175)
-t.write("Welcome to The Race!", font=("Arialblack", 10))
-
-# colors = ['firebrick', 'dark orange', 'gold', 'dodger blue', 'green yellow', 'slate blue']
+# Create turtle
 colors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple']
 
 # go to the start of the line, create six turtles with rainbow color
@@ -52,15 +78,15 @@ if user_bet:
 while is_race_on:
 
     for turtle in all_turtles:
-        if turtle.xcor() > 220:
+        if turtle.xcor() > 180:
             is_race_on = False
-            # print(f"The winner in this race is {turtle.color()}")
+
             if user_bet == turtle.color():
                 print(f"You win! The winner is [{turtle.pencolor()}] turtle.")
             else:
                 print(f"You lose! The winner is [{turtle.pencolor()}]turtle.")
         # random movement, random step distance
-        turtle.forward(random.randint(0, 10))
+        turtle.forward(random.randint(0, 15))
 
-
+# Output
 screen.exitonclick()
