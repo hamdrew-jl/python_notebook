2adf2cf4f72a08cfa6ac28cb3d20a365d9ddbd23
Hamdrew <141601957+hamdrew-jl@users.noreply.github.com>
2023-09-07 09:09:31 +0800

    Update turtle_03_fantasy circle.py

--- 02-python-Intermediate/02.02-Turtle & GUI/turtle_03_fantasy circle.py
+++ 02-python-Intermediate/02.02-Turtle & GUI/turtle_03_fantasy circle.py
@@ -1,43 +1,50 @@
-# Method 1  
+# Method 1
 """interact with user"""
+
+
 from turtle import Turtle, Screen, colormode
 import random
-
-turtle = Turtle()
 colormode(255)
-mathew_the_tortuga.speed(0)
+turtle = Turtle()
 
 
-def random_color():
+def randomcolor():
+    """set a random RGB color tuple, return r,g,b"""
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     return r, g, b
 
 
-turn = int(input("How much do you want the turtle to turn? (1 to 180)"))
-size_of_circle = int(input("How large do you want the circle? "))
-range_of_loop = int(360 / turn)
+def circle_graphic(size_c, angle_c):
+    for c in range(loop_number):
+        turtle.color(randomcolor())
+        turtle.circle(size_c)
+        turtle.left(angle_c)
 
-for i in range(range_of_loop):
-    mathew_the_tortuga.color(random_color())
-    mathew_the_tortuga.circle(size_of_circle)
-    mathew_the_tortuga.right(turn)
 
+angle = int(input("How much do you want the turtle to turn? (1 to 180)"))
+size = int(input("How large do you want the circle?"))
+loop_number = int(360 / angle)
+
+# change the speed
+turtle.speed("fastest")
+
+circle_graphic(size, angle)
+
+# Output
 screen = Screen()
 screen.exitonclick()
 
 
 # _______________________________________________________________________________
-# Method 2  
-"""use headng()"""
-# from turtle import Turtle, Screen
+# Method 2
+ """use heading()"""
+# from turtle import Turtle, Screen, colormode
 # import random
-# import turtle as t
 
 
-# # set colormode, or that will be string for default setting
-# t.colormode(255)
+# colormode(255)
 # turtle = Turtle()
 
 
@@ -49,29 +56,26 @@ screen.exitonclick()
 #     return r, g, b
 
 
-# def draw_spirograph(size):
-#     for i in range(int(360/size)):
+# def draw_spirograph(size_of_gap):
+#     for i in range(int(360 / size_of_gap)):
 #         turtle.circle(100)
 #         turtle.color(randomcolor())
 #         print(turtle.heading())
 
 #         # change the direction slight each time when draw circle
 #         current_heading = turtle.heading()
-#         turtle.setheading(current_heading + size)
+#         turtle.setheading(current_heading + size_of_gap)
 
 
 # # change the speed
 # turtle.speed('fastest')
 
-# turtle.circle(100)
-
-# draw_spirograph(10)
-
+# # turtle.circle(100)
+# draw_spirograph(5)
 
 # # Output
 # screen = Screen()
 # screen.exitonclick()
-
 
 
 # _______________________________________________________________________________
@@ -97,13 +101,12 @@ screen.exitonclick()
 #     for c in range(num):
 #         turtle.color(random_color())
 #         turtle.circle(50)
-#         turtle.left(360/num)
+#         turtle.left(360 / num)
 
 
 # turtle.speed("fastest")
 
 # draw_circlegraph(20)
-
 
 # screen = Screen()
 # screen.exitonclick()
